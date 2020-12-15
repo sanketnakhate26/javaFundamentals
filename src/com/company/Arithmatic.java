8b4b4c27e971a3c71e1fdf0433f25c8e5d8adbf1
Sanket Nakhate <sanketnakhate26@gmail.com>
2020-12-16 00:52:49 +0530

    Added arithmatic program

---
+++ src/com/company/Arithmatic.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Arithmatic {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+
+        System.out.println("a = "+ a + " and b = "+ b);
+        System.out.println("Sum = "+ (a+b) );
+        System.out.println("Minus = "+ (a-b) );
+        System.out.println("Multi = "+ (a*b) );
+        System.out.println("Div = "+ a/b );
+    }
+}

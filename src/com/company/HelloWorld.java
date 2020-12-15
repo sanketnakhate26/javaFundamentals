81f6d00e781e6d8205d51334c8c83b396301f46a
Sanket Nakhate <sanketnakhate26@gmail.com>
2020-12-16 00:42:04 +0530

    Added Hello world program

--- src/com/company/HelloWorld.java
+++ src/com/company/HelloWorld.java
@@ -1,8 +1,8 @@
 package com.company;
 
-public class Main {
+public class HelloWorld {
 
     public static void main(String[] args) {
-	// write your code here
+	System.out.println("Hello World");
     }
 }
